99172be8057f94024b055e073b106ef8a6a5e3dc
Troy Williams <troyw@waikato.ac.nz>
2012-10-05 11:40:00 +1300

    mod : assignment : coded added to hide standard assignment types, used to hide uploadsingle

--- mod/assignment/lib.php
+++ mod/assignment/lib.php
@@ -3964,6 +3964,9 @@ function assignment_get_types() {
 
     $standardassignments = array('upload','online','uploadsingle','offline');
     foreach ($standardassignments as $assignmenttype) {
+        if (!empty($CFG->{'assignment_hide_'.$assignmenttype})) {  // Not wanted
+            continue;
+        }
         $type = new stdClass();
         $type->modclass = MOD_CLASS_ACTIVITY;
         $type->type = "assignment&amp;type=$assignmenttype";
