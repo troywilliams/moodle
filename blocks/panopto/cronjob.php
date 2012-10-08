824b509712de5ef68d30a3583cf683d9b358f590
Troy Williams <troyw@waikato.ac.nz>
2012-10-08 15:09:42 +1300

    block : panopto : switched to new way of getting system context in cronjob file

--- blocks/panopto/cronjob.php
+++ blocks/panopto/cronjob.php
@@ -1,16 +1,17 @@
 <?php
+/**
+ * Cronjob for Panopto block
+ */
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once($CFG->dirroot .'/local/logger/logger.php');
 require_once($CFG->dirroot.'/blocks/moodleblock.class.php');
 require_once($CFG->dirroot.'/blocks/panopto/block_panopto.php');
 
-if (isset($_SERVER['REMOTE_ADDR'])) {
-    require_login();
-    require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM, SITEID));
-}
+require_login();
+$syscontext = context_system::instance();
+require_capability('moodle/site:config', $syscontext);
 $block = new block_panopto();
 $logger =& logger_get_logger('Panopto', 'FolderSync');
 $logger->info('Processing Panopto folders...');
 $block->full_user_list_sync();
 $logger->info('Done.');
-?>
