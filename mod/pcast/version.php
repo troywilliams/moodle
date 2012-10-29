1046e7253168b94f1468d2bc762303128f1c9d9b
Troy Williams <troyw@waikato.ac.nz>
2012-10-29 14:54:43 +1300

    mod : pcast : added back in uow upgrade code to increase intro field precision

--- mod/pcast/version.php
+++ mod/pcast/version.php
@@ -29,8 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2011080701;    // If version == 0 then module will not be installed
-//$module->version  = 2010032200;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011080702;    // If version == 0 then module will not be installed
 $module->requires = 2011070101;    // Requires this Moodle version
 $module->cron     = 0;             // Period for cron to check this module (secs)
 $module->component = 'mod_pcast';  // Full name of the plugin (used for diagnostics)
